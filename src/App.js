17e1c5c72b3f3959ce4a42e339bbbe943fd61b5c
Raveena Bandara <raveenabandara801@gmail.com>
2021-11-25 12:43:08 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import  './App.css';
+import './ProductData';
+import ProductData from './ProductData';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className = "Topbar">
+          <img src ='http://www.equiti.com/media/11666/amazon.jpg ' alt ='Amazon logo' />
+        </nav>
       </header>
+      <div className ="Maincontainer">
+        <div className="productPreview">
+          <img src = 'https://imgur.com/iOeUBV7.png'/>
+        </div>
+        <div className="productData">
+          <h1>FitBit 19 - The Smartest <br/> Watch</h1>
+          <p><b>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</b></p>
+          <h3>Select Colour</h3>
+          <div className ="productData">
+
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }

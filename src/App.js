db640784ae06672418e60b71a04d2196d7bf024d
Raveena Bandara <raveenabandara801@gmail.com>
2021-11-25 15:45:45 +0530

    add few files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './ProductData';
 import ProductData from './ProductData';
 
 
+
 function App() {
   return (
     <div className="App">
@@ -17,12 +18,17 @@ function App() {
           <img src = 'https://imgur.com/iOeUBV7.png'/>
         </div>
         <div className="productData">
-          <h1>FitBit 19 - The Smartest <br/> Watch</h1>
-          <p><b>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</b></p>
-          <h3>Select Colour</h3>
-          <div className ="productData">
+          <h1 className="productTitle">{ProductData.title}</h1>
+          <p className="productDescription">{ProductData.description}</p>
+          <h3 className="sectionHeading">Select Colour</h3>
+          <div>
+            <img className= "productImage,selectedProductImage" src='https://imgur.com/iOeUBV7.png' alt="Black Colored"/>
+            <img className="productImage" src='https://imgur.com/PTgQlim.png' alt="Red Colored"/>
+            <img className="productImage" src='https://imgur.com/Mplj1YR.png' alt="Blue Colored"/>
+            <img className="productImage" src='https://imgur.com/xSIK4M8.png' alt="Purple Colored"/>
 
           </div>
+
         </div>
 
       </div>
